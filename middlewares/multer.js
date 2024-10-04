336af1a79ba1723772bd0cd3660dd0c23b619a44
Shivansh <shivanshawasthi33@gmail.com>
2024-10-05 02:32:19 +0530

    multer setup

---
+++ middlewares/multer.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+
+// multer disk storage
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + file.originalName;
+        cb(null, uniqueSuffix)
+    }
+})
+
+export const upload = multer({ storage });
+
+
+
+
+
+
