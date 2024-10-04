875d98dbdadf9588c9d332cbe5137076a1d0bb2f
Shivansh <shivanshawasthi33@gmail.com>
2024-10-05 02:38:02 +0530

    multer uploads fix

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/tmp/my-uploads')
+        cb(null, './uploads')
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + file.originalName;
