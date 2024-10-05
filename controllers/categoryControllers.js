ca6dc84dcafb7a26bc1442e301172344b17cba83
Shivansh <shivanshawasthi33@gmail.com>
2024-10-05 20:26:39 +0530

    category controllers

---
+++ controllers/categoryControllers.js
@@ -0,0 +1,25 @@
+const Category = require('../models/Category');
+
+const createCategory = async (req, res) => {
+    try {
+        const { name } = req.body;
+        const category = new Category({ name });
+        await category.save();
+        res.status(201).json(category);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find();
+        res.json(categories);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+
+
+module.exports = { createCategory, getCategories };
