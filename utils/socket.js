04b931066895f13bea07ee68ae3c3044865a13ff
Shivansh <shivanshawasthi33@gmail.com>
2024-10-05 21:11:03 +0530

    socket.io

---
+++ utils/socket.js
@@ -0,0 +1,17 @@
+const { Server } = require('socket.io');
+
+const socketHandler = (server) => {
+    const io = new Server(server);
+
+    io.on('connection', (socket) => {
+        console.log('New socket connection');
+
+        socket.on('disconnect', () => {
+            console.log('Socket disconnected');
+        });
+    });
+
+    return io;
+};
+
+module.exports = socketHandler;
