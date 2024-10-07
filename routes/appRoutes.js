99e23d04efecdadb5ef6931eb82a504ff1edf938
Shivansh <shivanshawasthi33@gmail.com>
2024-10-08 01:01:57 +0530

    gell all apps routes

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const upload = require("../middlewares/multer");
-const { createApp } = require('../controllers/appControllers');
+const { createApp, getAllApps } = require('../controllers/appControllers');
 
 
 
-
+//--- ADMIN PANEL --- create an app
 router.post("/admin/create", upload.fields([
     { name: 'thumbnail', maxCount: 20 }
-])
-    , createApp);
+]), createApp);
+
+// get all apps
+router.get("/all", getAllApps)
 
 
 module.exports = router;
