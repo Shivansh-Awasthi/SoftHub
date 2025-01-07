ce49169531d62c6bf0c41704bf62d77046c8d8ed
Shivansh <shivanshawasthi33@gmail.com>
2025-01-08 02:57:18 +0530

    fix

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/Softhub`)
         console.log(`MongDb is connected, Host is: ${connectionInstance.connection.host}`);
 
     } catch (error) {
