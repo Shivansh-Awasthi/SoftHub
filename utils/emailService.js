a68b8a617c2d5cf6c8021097f0d6bfb80e01eea8
Shivansh <shivanshawasthi33@gmail.com>
2025-10-24 04:39:00 +0530

    added zoho mail

--- utils/emailService.js
+++ utils/emailService.js
@@ -2,12 +2,18 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Zoho SMTP transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.zoho.in',
+    port: 465,
+    secure: true,      // Use TLS, not SSL
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false  // Bypass SSL certificate validation
+    }
 });
 
 /**
